95251257e214148d5654d5b72f5440e4eb631148
MiltonDuarteReyes <37226285+MiltonADR@users.noreply.github.com>
2022-08-18 20:46:49 -0300

    Agregando  Header

--- proyectoIntegrador/src/app/app.module.ts
+++ proyectoIntegrador/src/app/app.module.ts
@@ -3,10 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { LogoAppComponent } from './components/logo-app/logo-app.component';
+import { SocialComponent } from './components/social/social.component';
+import { BannerComponent } from './components/banner/banner.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LogoAppComponent,
+    SocialComponent,
+    BannerComponent
+    
   ],
   imports: [
     BrowserModule,
