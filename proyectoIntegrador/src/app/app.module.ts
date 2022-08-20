283faff43206ec5ecb891f8ebf2323c4c6883601
MiltonDuarteReyes <37226285+MiltonADR@users.noreply.github.com>
2022-08-20 00:13:57 -0300

    Agregado del componente Acerca de Mi

--- proyectoIntegrador/src/app/app.module.ts
+++ proyectoIntegrador/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { HeaderComponent } from './components/header/header.component';
 import { LogoAppComponent } from './components/logo-app/logo-app.component';
 import { SocialComponent } from './components/social/social.component';
 import { BannerComponent } from './components/banner/banner.component';
+import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { BannerComponent } from './components/banner/banner.component';
     HeaderComponent,
     LogoAppComponent,
     SocialComponent,
-    BannerComponent
+    BannerComponent,
+    AcercaDeComponent
     
   ],
   imports: [
