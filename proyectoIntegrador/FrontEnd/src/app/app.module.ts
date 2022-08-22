e426aa8200dfb677cae392b524630035f5ac5317
MiltonDuarteReyes <37226285+MiltonADR@users.noreply.github.com>
2022-08-22 19:48:02 -0300

    Creacion de la carpeta FrontEnd
    se creo la carpeta FrontEnd

--- proyectoIntegrador/FrontEnd/src/app/app.module.ts
+++ proyectoIntegrador/FrontEnd/src/app/app.module.ts
@@ -5,7 +5,6 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { LogoAppComponent } from './components/logo-app/logo-app.component';
-import { SocialComponent } from './components/social/social.component';
 import { BannerComponent } from './components/banner/banner.component';
 import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
 import { ExperienciaComponent } from './components/experiencia/experiencia.component';
@@ -21,7 +20,6 @@ import { FooterComponent } from './components/footer/footer.component';
     AppComponent,
     HeaderComponent,
     LogoAppComponent,
-    SocialComponent,
     BannerComponent,
     AcercaDeComponent,
     ExperienciaComponent,
